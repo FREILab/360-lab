467887e2549d21c35d0e40c658cd60feb7c05b07
maximilian.schubert@telekom.de <maximilian.schubert@telekom.de>
2025-01-31 18:51:10 +0100

    feat: freilab 360 tour app files

--- generated/app-files/data.js
+++ generated/app-files/data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-1",
-      "name": "1",
+      "id": "0-allgemeiner-bereich",
+      "name": "Allgemeiner Bereich",
       "levels": [
         {
           "tileSize": 256,
@@ -20,24 +20,108 @@ var APP_DATA = {
         {
           "tileSize": 512,
           "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": -2.521455355357004,
+        "pitch": 0.3577768808964059,
+        "fov": 1.3536620968836544
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.958173073110739,
+          "pitch": 0.20675458941499514,
+          "rotation": 0,
+          "target": "3-lasercutter"
+        },
+        {
+          "yaw": -0.9525699768970952,
+          "pitch": 0.2316409694974766,
+          "rotation": 0,
+          "target": "4-schweien--terrasse-"
+        },
+        {
+          "yaw": 1.450669543210883,
+          "pitch": 0.23545165936666734,
+          "rotation": 0,
+          "target": "1-elektronik---nhwerkstatt"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.16182003634110842,
+          "pitch": 0.20880234699644085,
+          "title": "Küche",
+          "text": "<br>"
+        },
+        {
+          "yaw": -2.625288166551041,
+          "pitch": -0.08986276870109577,
+          "title": "Fahrradwerkstatt",
+          "text": "<div><br></div>"
+        },
+        {
+          "yaw": -2.7068296449326823,
+          "pitch": 0.7039514592925631,
+          "title": "Aufenthaltsbereich",
+          "text": "<br>"
+        },
+        {
+          "yaw": 2.270514099960419,
+          "pitch": -0.06244981396458549,
+          "title": "3D Drucker",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "1-elektronik---nhwerkstatt",
+      "name": "Elektronik- & Nähwerkstatt",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
         },
         {
           "tileSize": 512,
-          "size": 4096
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
         }
       ],
-      "faceSize": 2816,
+      "faceSize": 1632,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": 0.39580097017974936,
+        "pitch": 0.35431655456588196,
+        "fov": 1.3536620968836544
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": -0.4762500896687918,
+          "pitch": 0.34316956199747395,
+          "rotation": 12.566370614359176,
+          "target": "2-meetingraum"
+        },
+        {
+          "yaw": 1.3345033372230812,
+          "pitch": 0.3411518955600634,
+          "rotation": 6.283185307179586,
+          "target": "0-allgemeiner-bereich"
+        }
+      ],
       "infoHotspots": []
     },
     {
-      "id": "1-3",
-      "name": "3",
+      "id": "2-meetingraum",
+      "name": "Meetingraum",
       "levels": [
         {
           "tileSize": 256,
@@ -55,27 +139,418 @@ var APP_DATA = {
         {
           "tileSize": 512,
           "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": -0.5471280027607452,
+        "pitch": 0.45931186233837273,
+        "fov": 1.3536620968836544
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3922511203724408,
+          "pitch": 0.2959060104133062,
+          "rotation": 0,
+          "target": "1-elektronik---nhwerkstatt"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-lasercutter",
+      "name": "Lasercutter",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
         },
         {
           "tileSize": 512,
-          "size": 4096
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
         }
       ],
-      "faceSize": 2816,
+      "faceSize": 1632,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": -0.5247821918917808,
+        "pitch": 0.3042247576566819,
+        "fov": 1.3536620968836544
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": -2.87184634561212,
+          "pitch": 0.307403365864376,
+          "rotation": 0,
+          "target": "0-allgemeiner-bereich"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-schweien--terrasse-",
+      "name": "Schweißen & Terrasse ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": 0.016129820101365766,
+        "pitch": 0.3024653514586806,
+        "fov": 1.3536620968836544
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1831510943475365,
+          "pitch": 0.25589547217768605,
+          "rotation": 5.497787143782138,
+          "target": "0-allgemeiner-bereich"
+        },
+        {
+          "yaw": 2.6066962746924878,
+          "pitch": 0.2917881363194681,
+          "rotation": 0.7853981633974483,
+          "target": "5-holzwerkstatt-i"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-holzwerkstatt-i",
+      "name": "Holzwerkstatt I",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": -0.02169927416477435,
+        "pitch": 0.08731834102392533,
+        "fov": 1.3536620968836544
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8543755351941442,
+          "pitch": 0.11168006760031979,
+          "rotation": 10.995574287564278,
+          "target": "4-schweien--terrasse-"
+        },
+        {
+          "yaw": 0.944428453983992,
+          "pitch": 0.2159375277906399,
+          "rotation": 13.351768777756625,
+          "target": "7-holzwerkstatt-ii"
+        },
+        {
+          "yaw": 0.5037775508270723,
+          "pitch": 0.10809626280591189,
+          "rotation": 0,
+          "target": "8-metallwerkstatt-i"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-holzwerkstatt-iii",
+      "name": "Holzwerkstatt III",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": -0.924362767347688,
+        "pitch": 0.2592560155360033,
+        "fov": 1.3536620968836544
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.924362767347688,
+          "pitch": 0.2592560155360033,
+          "rotation": 0,
+          "target": "7-holzwerkstatt-ii"
+        },
+        {
+          "yaw": -0.5788407139013962,
+          "pitch": 0.08712022633459071,
+          "rotation": 0,
+          "target": "4-schweien--terrasse-"
+        },
+        {
+          "yaw": -0.261129670016615,
+          "pitch": 0.15661271540562893,
+          "rotation": 1.5707963267948966,
+          "target": "8-metallwerkstatt-i"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-holzwerkstatt-ii",
+      "name": "Holzwerkstatt II",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": -1.4727837005407949,
+        "pitch": 0.39968406107903576,
+        "fov": 1.3536620968836544
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.722805348048471,
+          "pitch": 0.3005819076365128,
+          "rotation": 0,
+          "target": "6-holzwerkstatt-iii"
+        },
+        {
+          "yaw": 0.9489094539548812,
+          "pitch": 0.2420299187574706,
+          "rotation": 0,
+          "target": "10-metallwerkstatt-iii"
+        },
+        {
+          "yaw": 1.7941464834290652,
+          "pitch": 0.13407307582255967,
+          "rotation": 0,
+          "target": "4-schweien--terrasse-"
+        },
+        {
+          "yaw": 2.967774670729635,
+          "pitch": 0.25447342955109953,
+          "rotation": 0,
+          "target": "8-metallwerkstatt-i"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-metallwerkstatt-i",
+      "name": "Metallwerkstatt I",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": -0.098019676000618,
+        "pitch": 0.286909990526512,
+        "fov": 1.3536620968836544
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7976245029196747,
+          "pitch": 0.2081521110164033,
+          "rotation": 0.7853981633974483,
+          "target": "9-metallwerkstatt-ii"
+        },
+        {
+          "yaw": -0.06518907947869579,
+          "pitch": 0.14415545346536973,
+          "rotation": 0,
+          "target": "10-metallwerkstatt-iii"
+        },
+        {
+          "yaw": -1.762484204150212,
+          "pitch": 0.21180339443756857,
+          "rotation": 4.71238898038469,
+          "target": "7-holzwerkstatt-ii"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-metallwerkstatt-ii",
+      "name": "Metallwerkstatt II",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": -0.021092841671006113,
+        "pitch": 0.20221969211808144,
+        "fov": 1.3536620968836544
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5193844135174768,
+          "pitch": 0.12380497591721173,
+          "rotation": 4.71238898038469,
+          "target": "7-holzwerkstatt-ii"
+        },
+        {
+          "yaw": -0.9285287548905288,
+          "pitch": 0.4237686152481963,
+          "rotation": 0,
+          "target": "8-metallwerkstatt-i"
+        },
+        {
+          "yaw": 0.9227524666347993,
+          "pitch": 0.2650713589393181,
+          "rotation": 0,
+          "target": "10-metallwerkstatt-iii"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-metallwerkstatt-iii",
+      "name": "Metallwerkstatt III",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": 0.04838946030410085,
+        "pitch": 0.23160537057380814,
+        "fov": 1.3536620968836544
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6705012057147783,
+          "pitch": 0.24730255735549633,
+          "rotation": 5.497787143782138,
+          "target": "9-metallwerkstatt-ii"
+        },
+        {
+          "yaw": -0.15104540605607397,
+          "pitch": 0.221125501766668,
+          "rotation": 0.7853981633974483,
+          "target": "8-metallwerkstatt-i"
+        },
+        {
+          "yaw": 0.4653462632691596,
+          "pitch": 0.17648557472308646,
+          "rotation": 0,
+          "target": "7-holzwerkstatt-ii"
+        }
+      ],
       "infoHotspots": []
     }
   ],
-  "name": "FreiLab",
+  "name": "FreiLAB",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
-    "fullscreenButton": true,
+    "fullscreenButton": false,
     "viewControlButtons": true
   }
 };
